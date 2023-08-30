de872bd779f61fccab993b4a1e84b1aadf3519fc
harry <opensourcebyharry@gmail.com>
2023-08-30 16:33:30 -0500

    async server socket with error handling

--- echo_server/server_socket_asyncio.py
+++ echo_server/server_socket_asyncio.py
@@ -1,4 +1,5 @@
-import asyncio
+import asyncio, signal
+import logging
 import socket
 import time
 
@@ -10,17 +11,29 @@ async def accept_client(server_socket):
         client_connection.setblocking(False)
         print(f"Received client {client_address}")
 
-        ## bug w.r.t error handling 
         asyncio.create_task(read_client(loop, client_connection))
 
 async def read_client(loop: asyncio.AbstractEventLoop, client_connection) -> None:
-    buffer = b''
-    data = await loop.sock_recv(client_connection, 1024)
-    if not data:
-        buffer += data
-    print(f"Got data {data}")
-    result = await loop.sock_sendall(client_connection, buffer)
-    return result
+    try:
+        buffer = b''
+        data = await loop.sock_recv(client_connection, 1024)
+        if not data:
+            buffer += data
+        print(f"Got data {data}")
+        if data == b"boom\r\n":
+            raise Exception("Unexpected network error")
+        result = await loop.sock_sendall(client_connection, buffer)
+        return result
+    except Exception as e:
+        logging.exception(e)
+    finally:
+        client_connection.close()
+
+
+def cancel_tasks():
+    print('Received a signal SIGINT')
+    tasks = asyncio.all_tasks()
+    [task.cancel() for task in tasks]
 
 async def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -29,6 +42,7 @@ async def main():
     address = ('127.0.0.1', 5000)
     server_socket.bind(address)
     server_socket.listen()
+    asyncio.get_running_loop().add_signal_handler(signal.SIGINT, cancel_tasks)
 
     await accept_client(server_socket)
 
