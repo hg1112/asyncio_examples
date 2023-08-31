a64e710593121b89351978aea19e36ff5246232c
harry <opensourcebyharry@gmail.com>
2023-08-30 20:15:46 -0500

    wait - allCompleted,firstException,firstCompleted

--- async_http/concurrent_request.py
+++ async_http/concurrent_request.py
@@ -1,8 +1,10 @@
 import asyncio
+import logging
+from concurrent.futures import FIRST_COMPLETED, FIRST_EXCEPTION, as_completed
 import aiohttp
 from aiohttp import ClientSession
 from util import delay, fetch_status, async_timed
-
+from util.async_http import fetch_status_delayed
 
 # Example 1 : Gen coroutines and await them 
 ## gather returns iff all of coroutines (wrapped as tasks) finish
@@ -38,13 +40,134 @@ from util import delay, fetch_status, async_timed
 # asyncio.run(main())
 
 # Example 4 : return exceptions (fail but graceful collection of errors)
+# @async_timed()
+# async def main():
+#     urls = ['https://nouchurl.com', 'https://www.example.com']
+#     async with aiohttp.ClientSession() as session:
+#         requests = [fetch_status(session, url) for url in urls]
+#         error, valid = await asyncio.gather(*requests, return_exceptions=True)
+#         print(valid)
+#         print(error)
+
+# asyncio.run(main())
+
+# Example 5: Uneven distributed response times
+# @async_timed()
+# async def main():
+#     async with aiohttp.ClientSession() as session:
+#         fetches = [
+#                 fetch_status(session, 'http://www.example.com'),
+#                 fetch_status_delayed(session, 'http://www.example.com', 10),
+#                 fetch_status_delayed(session, 'http://www.example.com', 5)
+#                 ]
+#         for finished in asyncio.as_completed(fetches):
+#             print(await finished)
+#     
+# asyncio.run(main())
+
+# Example 5: Uneven distributed response times with timeout
+# @async_timed()
+# async def main():
+#     async with aiohttp.ClientSession() as session:
+#         fetches = [
+#                 fetch_status(session, 'http://www.example.com'),
+#                 fetch_status_delayed(session, 'http://www.example.com', 10),
+#                 fetch_status_delayed(session, 'http://www.example.com', 5)
+#                 ]
+#         for finished in asyncio.as_completed(fetches, timeout=6):
+#             try:
+#                 result = await finished
+#                 print(result)
+#             except asyncio.TimeoutError:
+#                 print('Timeout error')
+#         for task in asyncio.all_tasks():
+#             print(task)
+
+#     
+#     
+# asyncio.run(main())
+
+
+# Example 6: wait - default 
+
+# @async_timed()
+# async def main():
+#     async with ClientSession() as session:
+#         fetches = [
+#                 asyncio.create_task(fetch_status(session, 'https://www.example.com')),
+#                 asyncio.create_task(fetch_status(session, 'https://www.example.com'))
+#                 ]
+#         done, pending = await asyncio.wait(fetches)
+#         print(f'Completed {len(done)} tasks')
+#         print(f'Pending {len(pending)} tasks')
+
+## done_task is guaranteed a completed future (success/failure)
+#         for done_task in done:
+#             result = await done_task
+#             print(result)
+
+# asyncio.run(main())
+
+
+# Example 7: wait - ALL_COMPLETED
+# @async_timed()
+# async def main():
+#     async with ClientSession() as session:
+#         good = fetch_status(session, 'https://www.example.com')
+#         bad = fetch_status(session, 'http://nosuchurl')
+#         fetches = [
+#                 asyncio.create_task(good),
+#                 asyncio.create_task(bad)
+#                 ]
+#         done, pending = await asyncio.wait(fetches)
+#         print(f'Completed {len(done)} tasks')
+#         print(f'Pending {len(pending)} tasks')
+#         for done_task in done:
+
+#             if done_task.exception():
+#                 logging.error("Request faild with exception ", exc_info=done_task.exception())
+#             else:
+#                 print(f"Success : {done_task.result()}")
+
+# asyncio.run(main())
+
+# Example 8 : wait - FIRST_EXCEPTION
+# @async_timed()
+# async def main():
+#     async with ClientSession() as session:
+#         
+#         fetches = [
+#                 asyncio.create_task(fetch_status(session,'pytohn://bad.com')),
+#                 asyncio.create_task(fetch_status_delayed(session, 'https://www.example.com', 3)),
+#                 asyncio.create_task(fetch_status_delayed(session, 'https://www.example.com', 3))
+#                 ]
+#         done, pending = await asyncio.wait(fetches, return_when=FIRST_EXCEPTION)
+#         print(f'Completed {len(done)} tasks')
+#         print(f'Pending {len(pending)} tasks')
+#         for done_task in done:
+#             if done_task.exception():
+#                 logging.error("Request faild with exception ", exc_info=done_task.exception())
+#             else:
+#                 print(f"Success : {done_task.result()}")
+#         for task in pending:
+#             task.cancel()
+# asyncio.run(main())
+
+# Example 8 : wait - FIRST_COMPLETED , but process others as they complete similar to as_completed
 @async_timed()
 async def main():
-    urls = ['https://nouchurl.com', 'https://www.example.com']
-    async with aiohttp.ClientSession() as session:
-        requests = [fetch_status(session, url) for url in urls]
-        error, valid = await asyncio.gather(*requests, return_exceptions=True)
-        print(valid)
-        print(error)
-
+    async with ClientSession() as session:
+        
+        pending = [
+                asyncio.create_task(fetch_status(session, 'https://www.example.com')),
+                asyncio.create_task(fetch_status(session, 'https://www.example.com')),
+                asyncio.create_task(fetch_status(session, 'https://www.example.com'))
+                ]
+        while pending:
+            done, pending = await asyncio.wait(pending, return_when=FIRST_COMPLETED)
+            print(f'Completed {len(done)} tasks')
+            print(f'Pending {len(pending)} tasks')
+            for done_task in done:
+                print(await done_task)
 asyncio.run(main())
+
--- util/async_http.py
+++ util/async_http.py
@@ -1,5 +1,11 @@
 from aiohttp import ClientSession
+from util import delay
 
 async def fetch_status(session: ClientSession, url: str) -> int:
+    async with session.get(url) as result:
+        return result.status
+
+async def fetch_status_delayed(session: ClientSession, url: str, delay_seconds: int) -> int:
+    await delay(delay_seconds)
     async with session.get(url) as result:
         return result.status
